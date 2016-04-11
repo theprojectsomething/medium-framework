6a87d9f85a864dd0d8e4e69e52d44df1cfe90a53
Som Meaden <som@withcollective.com>
2016-04-11 15:16:59 +1000

    include search

--- assets/js/docs-nav.js
+++ assets/js/docs-nav.js
@@ -13,7 +13,12 @@ MediumFramework.View.extend({
         set: 'innerHTML',
       },
 
-      "$el:list": '[data-nav]'
+      ".menu [name='search']": {
+        to: "search",
+      },
+
+      "$el:list": '[data-nav]',
+      "$el:search": '.menu [name="search"]'
 
     }
   },
@@ -26,23 +31,62 @@ MediumFramework.View.extend({
       this.bind();
 
       this.trigger('$el:list');
+      this.on("search", this.fn.on.search)
+
     },
 
     on: {
       
       add: function (list) {
+        this.props.items = [];
         return list.reduce(function (html, $el) {
           var text = $el.textContent.replace(/ \(.*$/, '');
-          $el.id = $el.id || text.replace(/^_/, 'utility')
+              $el.id = $el.id || text.replace(/^_/, 'utility')
                        .replace(/^\$[(.]/, 'dom-')
                        .replace(/(\( ?)|( ?\))/g, '')
                        .replace(/[^$\w]+/g, '-')
-                       .toLowerCase();
+                       .replace(/(^-+)|(-+$)/g, '')
+                       .toLowerCase(),
+              search = text.replace(/\b(the|and|or)\b\W/g, '') + " " + ($el.getAttribute("data-search") || "");
+          
+          this.props.items.push({
+            search: search.toLowerCase(),
+            id: $el.id,
+            $el: $el.parentElement
+          });
+
           return html + `
           <li class="tag-${$el.tagName}">
             <a href="#${$el.id}">${text}</a>
           </li>`;
         }.bind( this ), '');
+      },
+
+      search: function (search) {
+        if(search.length < 2) search = false;
+
+        $('.search--match, .search--nomatch').removeClass('search--match search--nomatch');
+
+        var matches = 0;
+        if(search) {
+          this.props.items.forEach(function (item) {
+            if(item.search.indexOf(search.toLowerCase())<0) {
+              item.$el.addClass('search--nomatch');
+              $('.menu [href="#' + item.id + '"]')[0].parentElement.addClass('search--nomatch');
+            } else {
+              item.$el.addClass('search--match');
+              var $el = item.$el;
+              while($el) {
+                $el.removeClass('search--nomatch');
+                $el = $el.parentElement;
+              }
+              ++matches;
+            }
+          });
+        }
+
+        this.props.$el.search.eq(0).parentNode.attr('data-matches', search ? matches : -1);
+        $('body').toggleClass('search', matches);
       }
     }
   }
